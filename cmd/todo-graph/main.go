564bd9338fa1fdd313b456f3b7f93a37ad1c9749
kuri-sun <harukikuriwada@gmail.com>
2025-12-18 17:21:16 +0900

    Add tree view option to scan output

--- cmd/todo-graph/main.go
+++ cmd/todo-graph/main.go
@@ -4,9 +4,11 @@ import (
 	"fmt"
 	"os"
 	"path/filepath"
+	"sort"
 	"strings"
 
 	"todo-graph/internal/engine"
+	"todo-graph/internal/graph"
 )
 
 var version = "dev"
@@ -22,7 +24,17 @@ func main() {
 	cmd := os.Args[1]
 	switch cmd {
 	case "scan":
-		os.Exit(runScan(p))
+		showTree := false
+		for _, arg := range os.Args[2:] {
+			switch arg {
+			case "--tree", "-t":
+				showTree = true
+			default:
+				fmt.Fprintf(os.Stderr, "unknown flag for scan: %s\n", arg)
+				os.Exit(1)
+			}
+		}
+		os.Exit(runScan(p, showTree))
 	case "check":
 		os.Exit(runCheck(p))
 	case "visualize":
@@ -36,7 +48,7 @@ func main() {
 	}
 }
 
-func runScan(p printer) int {
+func runScan(p printer, showTree bool) int {
 	root, err := currentRoot()
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "failed to resolve working directory: %v\n", err)
@@ -72,6 +84,16 @@ func runScan(p printer) int {
 	p.resultLine(true)
 	p.infof("todos : %d", len(graph.Todos))
 	p.infof("genereated: %s", filepath.Join(root, ".todo-graph"))
+	if roots := findRoots(graph); len(roots) > 0 {
+		p.infof("roots : %s", strings.Join(roots, ", "))
+	}
+	if showTree {
+		fmt.Println()
+		p.section("Graph (tree)")
+		for _, line := range renderTree(graph) {
+			fmt.Println(line)
+		}
+	}
 	return 0
 }
 
@@ -173,6 +195,75 @@ func runVisualize(args []string) int {
 	return 0
 }
 
+func findRoots(g graph.Graph) []string {
+	indegree := make(map[string]int, len(g.Todos))
+	for id := range g.Todos {
+		indegree[id] = 0
+	}
+	for _, e := range g.Edges {
+		indegree[e.To]++
+	}
+	var roots []string
+	for id, d := range indegree {
+		if d == 0 {
+			roots = append(roots, id)
+		}
+	}
+	sort.Strings(roots)
+	return roots
+}
+
+func renderTree(g graph.Graph) []string {
+	adj := make(map[string][]string)
+	for _, e := range g.Edges {
+		adj[e.From] = append(adj[e.From], e.To)
+	}
+	for k := range adj {
+		sort.Strings(adj[k])
+	}
+
+	roots := findRoots(g)
+	if len(roots) == 0 {
+		for id := range g.Todos {
+			roots = append(roots, id)
+		}
+		sort.Strings(roots)
+	}
+
+	var lines []string
+	visited := make(map[string]bool)
+
+	var dfs func(id string, depth int, stack map[string]bool)
+	dfs = func(id string, depth int, stack map[string]bool) {
+		t, ok := g.Todos[id]
+		location := ""
+		if ok {
+			location = fmt.Sprintf(" (%s:%d)", t.File, t.Line)
+		}
+		prefix := strings.Repeat("    ", depth)
+		if stack[id] {
+			lines = append(lines, fmt.Sprintf("%s- [ ] %s%s [cycle]", prefix, id, location))
+			return
+		}
+		if visited[id] && depth > 0 {
+			lines = append(lines, fmt.Sprintf("%s- [ ] %s%s [seen]", prefix, id, location))
+			return
+		}
+		lines = append(lines, fmt.Sprintf("%s- [ ] %s%s", prefix, id, location))
+		stack[id] = true
+		visited[id] = true
+		for _, next := range adj[id] {
+			dfs(next, depth+1, stack)
+		}
+		delete(stack, id)
+	}
+
+	for _, r := range roots {
+		dfs(r, 0, make(map[string]bool))
+	}
+	return lines
+}
+
 func currentRoot() (string, error) {
 	root, err := os.Getwd()
 	if err != nil {
@@ -186,6 +277,7 @@ func printHelp() {
 	fmt.Println()
 	fmt.Println("Usage:")
 	fmt.Println("  todo-graph scan       Scan repository and update .todo-graph")
+	fmt.Println("      --tree, -t        Print tree view of graph")
 	fmt.Println("  todo-graph check      Validate TODO graph consistency")
 	fmt.Println("  todo-graph visualize  Output graph in a given format")
 }
